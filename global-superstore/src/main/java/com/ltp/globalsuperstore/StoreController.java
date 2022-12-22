a815aa2973624d55c2d99a316afb059a1db5d4dc
Ozi <oguzkaanyildiz@mac-ozi.local>
2022-12-22 11:49:28 +0100

    Add Item class, bind form, create post method

--- global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -1,14 +1,21 @@
 package com.ltp.globalsuperstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StoreController {
 
+    List<Item> items = new ArrayList<Item>();
+
     @GetMapping("/")
     public String getForm(Model model) {
+        model.addAttribute("item", new Item());
         model.addAttribute("categories", Constants.CATEGORIES);
         return "index";
     }
@@ -16,5 +23,11 @@ public class StoreController {
     @GetMapping("/inventory")
     public String getInventory() {
         return "inventory";
+    }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(Item item) {
+        items.add(item);
+        return "redirect:/inventory";
     }
 }
